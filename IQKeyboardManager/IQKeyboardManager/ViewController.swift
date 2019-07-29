bba437b957ed745021a72abc7ee2e592b90e0f09
渡邉舜也 <shun.1630.effort@gmail.com>
2019-07-29 11:40:50 +0900

    IQKeyboardManagerを入れました

---
+++ IQKeyboardManager/IQKeyboardManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IQKeyboardManager
+//
+//  Created by 渡邉舜也 on 29/07/2019.
+//  Copyright © 2019 渡邉舜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
